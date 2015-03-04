69e8677b2dea2431662b43f75f048f2e05af4e4f
Roque Arnas <roque.arnas@didesweb.com>
2015-03-04 19:57:05 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Didesweb, proteger el correo de los spambots</title>
+	<!-- Didesweb -->  
+	<!-- Diseño y desarrollo web -->  
+	<!-- http://didesweb.com/-->  
+	<!-- Este obra está protegida bajo licencia Creative Commons Attribution --> 
+</head>
+<body>
+	<h1>&copy; Didesweb, proteger el correo de los spambots</h1>
+	<form action="funcion_validar_url.php" method="POST"> 
+	<input type="text" name="variable_input"/>
+	<input type="submit" value="enviar"/>
+	</form>
+</body>
+</html>
